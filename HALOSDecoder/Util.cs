41300ef2fb5984042ee5206d1847936ac56c9f78
Kevin <dragn194@gmail.com>
2017-07-03 20:20:23 -0400

    Add utility methods to convert to and from hex strings

---
+++ HALOSDecoder/Util.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HALOSDecoder
+{
+    public class Util
+    {
+
+        public static byte[] StringToByteArray(string strData)
+        {
+            return Enumerable.Range(0, strData.Length)
+                     .Where(x => x % 2 == 0)
+                     .Select(x => Convert.ToByte(strData.Substring(x, 2), 16))
+                     .ToArray();
+        }
+
+        public static string ByteArrayToString(byte[] byteData)
+        {
+            return BitConverter.ToString(byteData).Replace("-", "");
+        }
+
+    }
+}
