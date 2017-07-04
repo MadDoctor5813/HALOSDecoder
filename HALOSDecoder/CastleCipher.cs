1e9811e59a7f8540a8f70a575bf24e13a2bd7647
Kevin <dragn194@gmail.com>
2017-07-03 20:57:48 -0400

    Add an interface to BouncyCastle block ciphers

---
+++ HALOSDecoder/CastleCipher.cs
@@ -0,0 +1,41 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HALOSDecoder
+{
+
+    public class CastleCipher
+    {
+
+        public IBlockCipher Engine { get; set; }
+        public BufferedBlockCipher Cipher { get; set; }
+
+        public CastleCipher(IBlockCipher engine)
+        {
+            this.Engine = engine;
+        }
+
+        public void InitEcb(byte[] key)
+        {
+            Cipher = new BufferedBlockCipher(Engine);
+            Cipher.Init(false, new KeyParameter(key));
+        }
+
+        public void InitCbc(byte[] key, byte[] iv)
+        {
+            Cipher = new BufferedBlockCipher(new CbcBlockCipher(Engine));
+            Cipher.Init(false, new ParametersWithIV(new KeyParameter(key), iv));
+        }
+
+        public byte[] Decrypt(byte[] data)
+        {
+            return Cipher.DoFinal(data);
+        }
+    }
+}
