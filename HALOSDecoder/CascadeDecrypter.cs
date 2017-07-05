1f266ef0db8d2d663b7cba7cce2c352f9b5b9b33
Kevin <dragn194@gmail.com>
2017-07-04 21:45:57 -0400

    Add cascade decryption

---
+++ HALOSDecoder/CascadeDecrypter.cs
@@ -0,0 +1,44 @@
+﻿using Org.BouncyCastle.Crypto.Engines;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HALOSDecoder
+{
+    class CascadeDecrypter
+    {
+
+        public CascadeDecrypter()
+        {
+        }
+
+        public void CascadeDecrypt(string fullLogPath, string logPath)
+        {
+            using (StreamWriter fullLogWriter = new StreamWriter(new FileStream(fullLogPath, FileMode.Create, FileAccess.Write)))
+            using (StreamWriter logWriter = new StreamWriter(new FileStream(logPath, FileMode.Create, FileAccess.Write)))
+            {
+                CastleCipher serpent = new CastleCipher(new SerpentEngine());
+                CastleCipher twofish = new CastleCipher(new TwofishEngine());
+                CastleCipher aes = new CastleCipher(new AesEngine());
+                foreach (byte[] key in DataLoader.keys)
+                {
+                    serpent.InitEcb(key);
+                    twofish.InitEcb(key);
+                    aes.InitEcb(key);
+                    DecryptResult serpentResult = serpent.Decrypt(DataLoader.halosData);
+                    DecryptResult twofishResult = twofish.Decrypt(serpentResult.Plaintext);
+                    DecryptResult final = aes.Decrypt(twofishResult.Plaintext);
+                    final.WriteToFile(fullLogWriter);
+                    if (final.DistinctBytes < 180)
+                    {
+                        final.WriteToFile(logWriter);
+                    }
+                }
+            }
+        }
+
+    }
+}
--- HALOSDecoder/Program.cs
+++ HALOSDecoder/Program.cs
@@ -12,12 +12,19 @@ namespace HALOSDecoder
         const string fullLogPath = "decrypt_log_full.txt";
         const string logPath = "decrypt_log.txt";
 
+        const string fullCascadeLogPath = "cascade_log_full.txt";
+        const string cascadeLogPath = "cascade_log.txt";
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Starting mass decryption...");
             MassDecrypter decrypter = new MassDecrypter();
             decrypter.MassDecrypt(fullLogPath, logPath);
             Console.WriteLine($"Mass decrypt logs written to {fullLogPath} and {logPath}.");
+            Console.WriteLine("Starting cascade decryption...");
+            CascadeDecrypter cascade = new CascadeDecrypter();
+            cascade.CascadeDecrypt(fullCascadeLogPath, cascadeLogPath);
+            Console.WriteLine($"Cascade decrypt logs written to {fullCascadeLogPath} and {cascadeLogPath}.");
         }
 
     }
